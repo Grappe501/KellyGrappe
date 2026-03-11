e8fa565c7b3e98726e2b99a4f91208135cac45ae
Grappe501 <grappe4arkansas@gmail.com>
2026-03-11 12:36:41 -0600

    fix missing page imports

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,8 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import LiveContactsPage from "@modules/liveContact/LiveContactsListPage";
 import TeamSignupPage from "@modules/teamSignup/TeamSignupPage";
-import ThankYouPage from "@modules/common/ThankYouPage";
-import NotFoundPage from "@modules/common/NotFoundPage";
+import ThankYouPage from "@modules/teamSignup/ThankYouPage";
+import NotFoundPage from "@modules/teamSignup/NotFoundPage";
 
 export default function App() {
   return (
