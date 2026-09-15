3e361d897cccb0201da5588a301c3656a0d847ef
Grappe501 <grappe4arkansas@gmail.com>
2026-09-15 09:24:19 -0500

    feat: wire election information and polling operations routes

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import WarRoomDashboardPage from "./modules/dashboard/WarRoomDashboardPage";
+import ElectionInformationPage from "./modules/election/ElectionInformationPage";
+import PollingOperationsPage from "./modules/election/PollingOperationsPage";
 
 /**
  * IMPORTANT
@@ -12,13 +14,11 @@ import WarRoomDashboardPage from "./modules/dashboard/WarRoomDashboardPage";
 export default function App() {
   return (
     <Routes>
-
-      {/* Default landing */}
       <Route path="/" element={<WarRoomDashboardPage />} />
-
-      {/* War Room */}
       <Route path="/war-room" element={<WarRoomDashboardPage />} />
-
+      <Route path="/voter-center" element={<ElectionInformationPage />} />
+      <Route path="/elections" element={<ElectionInformationPage />} />
+      <Route path="/polling-operations" element={<PollingOperationsPage />} />
     </Routes>
   );
 }
